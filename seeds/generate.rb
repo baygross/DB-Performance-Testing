48fea93426a46de1a1604c50dd0f8287261f5799
Bay <bgross17@gmail.com>
2012-04-13 14:01:30 -0400

    progress, wrapping generate as a class

--- seeds/generate.rb
+++ seeds/generate.rb
@@ -1,148 +1,106 @@
 #!/usr/bin/env ruby
-
 require 'random_data'
 
-#--Configure ---------------------------------------
+Class Generate
+  #--Configure ---------------------------------------
 
-# App1 
-@num_hashtags = 10000
-@num_users = 100000
+  # App1 
+  @@num_hashtags = 10000
+  @@num_users = 100000
 
-@power_user = [100, 200]  #range of tweets
-@new_user = [0, 25]     #range of tweets
-@power_user_ratio = 0.5   #ratio of users that are 'power' 
+  @@power_user = [100, 200]  #range of tweets
+  @@new_user = [0, 25]     #range of tweets
+  @@power_user_ratio = 0.5   #ratio of users that are 'power' 
 
-# App2
+  # App2
 
+  #----Config Accessors---------------------------------
 
-#----------------------------------------------------
-
-def main
-
-  #find root and set local paths
-  @path = File.dirname(File.expand_path(__FILE__))
-  @data_path = @path + "/data"
-  @app1_path = @data_path + "/app1"
-  @app2_path = @data_path + "/app2"
-
-  #create directory tree if necessary
-  createDirectories
-  
-  #run our seed functions
-  seedApp1
-  seedApp2
-end
-
-
-# creates data/app1
-#         data/app2
-# if necessary
-def createDirectories
-
-  Dir::mkdir(@data_path) unless File.exists?(@data_path)
-  Dir::mkdir(@app1_path) unless File.exists?(@app1_path)
-  Dir::mkdir(@app2_path) unless File.exists?(@app2_path)
-
-end
-
-
-# This app is modeled after twitter
-# data will be stored plain text in 
-# the path 'data/app1'
-# files;
-#   users.txt
-#   tweets.txt
-#   hashtags.txt
-def seedApp1
-
-  createHashtags
-  createUsersAndTweets
-  
-end
-
-
-#creates our hashtag output file
-def createHashtags
-
-  File.open(@app1_path + '/' + 'hashtags.txt', 'w') do |ff|  
-    @num_hashtags.times do
-      ff << hashtag + "\n"
-    end   
+  #Config Accessors
+  def num_hashtags
+    @@num_hashtags
+  end
+  def num_users
+    @@num_users
+  end
+  def power_user
+    @@power_user
+  end
+  def new_user
+    @@new_user
+  end
+  def power_user_ratio
+    @@power_user_ratio
   end
 
-end  
-
-#generates a random hashtag
-#hashtags can (randomly) be 1,2 or 3 words long
-def hashtag
-
-  r= rand
-  ret = Random.paragraphs(1)
-  ret = ret.split  
-
-  if r > 0.7
-    ret = ret.sample
-  elsif r > 0.4 || ret.length < 4
-    ret = ret.sample.capitalize + ret.sample.capitalize
-  else
-    ret = ret.sample.capitalize + ret.sample.capitalize + ret.sample.capitalize
-  end
-
-  #TODO: make this a single gsub regex
-  ret.gsub('\'', '').gsub('\"', '').gsub('.', '').gsub('?', '').gsub('!', '').gsub(',', '')
+  #-----Methods------------------------------------------
   
-  ret
-end
+  #creates a twitter user object for APP1
+  def twitter_user
 
-# generates a random tweet
-# and ensures there is no \n at end
-def tweet
-
-  ret = Random.paragraphs(1)
-  if ret.length <= 140
-    ret = ret.slice(0.. -2)
-  else
-    ret = ret.slice(0, 140)
-  end
-
-end
-
-#creates our users and tweets files
-def createUsersAndTweets
-
-  File.open(@app1_path + '/' + 'users.txt', 'w') do |users|
-    File.open(@app1_path + '/' + 'tweets.txt', 'w') do |tweets|
-
-      #loop over all users
-      @num_users.times do |i|
-        users << i.to_s + " " + Random.firstname + " " + Random.lastname + " \"" + tweet + "\"\n"
-
-        #if the users is a power user or a new user
-        if rand < @power_user_ratio
-          num_tweets = (rand(@power_user[1]) + @power_user[0])
-        else 
-          num_tweets = (rand(@new_user[1]) + @new_user[0])
-        end
-
-        #output tweets for this user
-        num_tweets.times do |t|
-          #output user_id then everything else is the tweet!
-          tweets << i.to_s + " " + tweet + "\n"
-        end
-      end
-
+    #generate our new user
+    user = {
+      :fname => Random.firstname,
+      :lname => Random.lastname,
+      :bio => randTweet,
+      :tweets => []
+    }
+  
+    #decide if the user is a power user or a new user
+    if rand < @@power_user_ratio
+      num_tweets = (rand(@@power_user[1]) + @@power_user[0])
+    else 
+      num_tweets = (rand(@@new_user[1]) + @@new_user[0])
     end
+
+    #generate tweets for this user
+    num_tweets.times do |t|
+      user[:tweets] << randTweet
+    end
+    
+    #return the user!
+    user
+  end
+  
+  
+  #generates a random hashtag for app1
+  #hashtags can (randomly) be 1,2 or 3 words long
+  def twitter_hashtag
+
+    r= rand
+    ret = Random.paragraphs(1)
+    ret = ret.split  
+
+    if r > 0.7
+      ret = ret.sample
+    elsif r > 0.4 || ret.length < 4
+      ret = ret.sample.capitalize + ret.sample.capitalize
+    else
+      ret = ret.sample.capitalize + ret.sample.capitalize + ret.sample.capitalize
+    end
+
+    #TODO: make this a single gsub regex
+    ret.gsub('\'', '').gsub('\"', '').gsub('.', '').gsub('?', '').gsub('!', '').gsub(',', '')
+
+    ret
+  end
+  
+  #-----Privaet Methods------------------------------------
+  private
+  
+  # generates a random tweet
+  # and ensures there is no \n at end
+  def randTweet
+
+    ret = Random.paragraphs(1)
+    if ret.length <= 140
+      ret = ret.slice(0.. -2)
+    else
+      ret = ret.slice(0, 140)
+    end
+
   end
 
-end
-
-# This app is modeled after an online
-# forum and stored in 'data/app2'
-# files:
-def seedApp2
 
 end
 
-
-#run main
-main
--- seeds/pg.rb
+++ seeds/pg.rb
@@ -1,10 +1,24 @@
 #!/usr/bin/env ruby
 
 require 'pg'
+require 'generate.rb'
 
-#open connection to app1
-conn = PG.connect(dbname: 'app1')
+#
+# Initialize our connection to PG database
+#
+conn = PGconn.open({ 
+            host:  '',
+            port: '',
+            login: '',
+            password: '',
+            dbname: 'app1'
+       })
 
+
+#
+# Create our tables!
+#
+# TODO: can we make this indempotent?
 #create table for user that has first/last name and bio
 conn.exec('CREATE TABLE users(id SERIAL PRIMARY KEY, first_name VARCHAR(32), last_name VARCHAR(32), bio VARCHAR(140));')
 
@@ -18,67 +32,80 @@ conn.exec('CREATE TABLE hashtags(id SERIAL PRIMARY KEY, hashtag VARCHAR(140));')
 conn.exec('CREATE TABLE hashtags_tweets(hashtag_id Integer, tweet_id Integer);')
 
 
-#begin transaction
+#
+# Create Users and Tweets
+#
 conn.exec('BEGIN')
+  #add all of the users and tweets to the db
+  @num_users.times do |i|
 
-#add all of the users and tweets to the db
-@num_users.times do |i|
     #every 5000 iterations, commit to disk
     if i%5000==0
-        conn.exec('COMMIT')
-        conn.exec('BEGIN')
+      conn.exec('COMMIT')
+      conn.exec('BEGIN')
     end
-    #ret = call function that returns hash
-    #add users
-    conn.exec('INSERT INTO users(first_name, last_name, bio) VALUES ($1, $2, $3)', [ret.fname, ret.lname, ret.bio])
+
+    #get a new user from generate API
+    user = Generate.twitter_user
     
+    #add that user
+    conn.exec('INSERT INTO users(first_name, last_name, bio) VALUES ($1, $2, $3)', [user[:fname], user[:lname], user[:bio]])
+
     #add all of the user's tweets
     current_user=conn.exec("SELECT currval(pg_get_serial_sequence('users', 'id'));")
-    ret.tweets.each do |tweet|
-        conn.exec('INSERT INTO tweets(tweet, user_id) VALUES($1, $2)', [tweet, current_user])
+    user[:tweets].each do |tweet|
+      conn.exec('INSERT INTO tweets(tweet, user_id) VALUES($1, $2)', [tweet, current_user])
     end
-end
-
-#commit results
+  end
 conn.exec('COMMIT')
 
-#add the hashtags
-#begin transaction
-conn.exec('BEGIN')
 
-@num_hashtags.tims |i|
+#
+#  Create Hashtags
+#
+conn.exec('BEGIN')
+  @num_hashtags.times do |i|
     #every 5000 iterations, commit to disk
     if i%5000==0
-        conn.exec('COMMIT')
-        conn.exec('BEGIN')
+      conn.exec('COMMIT')
+      conn.exec('BEGIN')
     end
+
+    #get a hashtag from the Generate API class
+    hashtag = Generate.twitter_hashtag
     
     #add hashtag to table
     conn.exec('INSERT INTO hashtags(hashtag) VALUES ($1)', [hashtag])
-end
-
-#commit results
+  end
 conn.exec('COMMIT')
 
+
+#
+# Associate Hashtags and Tweets
+#
+#lookup our tweet and hashtag ranges for fast bulk insertion!
 min_tweet=conn.exec("SELECT MIN(id) FROM tweets;")
 max_tweet=conn.exec("SELECT MAX(id) FROM tweets;")
 min_hash=conn.exec("SELECT MIN(id) FROM hashtags;")
 max_hash=conn.exec("SELECT MIN(id) FROM hashtags;")
 
 conn.exec('BEGIN')
+  #loop over all tweets
+  for i in (min_tweet..max_tweet)
 
-#associate hashtags with tweets
-for i in (min_tweet..max_tweet)
+    #random 0-2 hashtags per tweet
     r=rand
-    
-    #add on hastag to this tweet
+
+    #add one hastag to this tweet
     if r < 1/3.to_f
-        conn.exec('INSERT INTO hashtags_tweets(hashtag_id, tweet_id) VALUES ($1, $2)', [i, (rand*(max_hash+1-min_hash)+min_hash).floor])
+      conn.exec('INSERT INTO hashtags_tweets(hashtag_id, tweet_id) VALUES ($1, $2)', [i, (rand*(max_hash+1-min_hash)+min_hash).floor])
     end
-    
+
     #add a second hashtag to this tweet
     if r < 2/3.to_f
-        conn.exec('INSERT INTO hashtags_tweets(hashtag_id, tweet_id) VALUES ($1, $2)', [i, (rand*(max_hash+1-min_hash)+min_hash).floor])
+      conn.exec('INSERT INTO hashtags_tweets(hashtag_id, tweet_id) VALUES ($1, $2)', [i, (rand*(max_hash+1-min_hash)+min_hash).floor])
     end
-end
+
+    #else no hashtags!
+  end
 conn.exec('COMMIT')
