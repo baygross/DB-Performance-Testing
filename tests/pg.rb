d3321d0a5b44200c79d6903d4d0fed731b483d1f
Charlie Croom <charlie.croom@gmail.com>
2012-04-27 20:34:03 -0400

    back to exec, rely on thread pool instead

--- tests/pg.rb
+++ tests/pg.rb
@@ -70,13 +70,13 @@ class PGTest
     
     #generate a new tweet
     body = "This is a new tweet being written to the DB!"
-    new_id = @db.async_exec('INSERT INTO tweets(tweet, user_id) VALUES($1, $2) RETURNING id;', [body, user_id])
+    new_id = @db.exec('INSERT INTO tweets(tweet, user_id) VALUES($1, $2) RETURNING id;', [body, user_id])
     new_id = new_id[0][0].to_i
     
     #insert 0-2 hashtags per tweet
     rand(2).times do 
       new_tag = rand(@max_hash - @min_hash + 1) + @min_hash
-      @db.async_exec('INSERT INTO hashtags_tweets(tweet_id, hashtag_id) VALUES ($1, $2)', [new_id, new_tag])
+      @db.exec('INSERT INTO hashtags_tweets(tweet_id, hashtag_id) VALUES ($1, $2)', [new_id, new_tag])
     end
     
     debug "wrote new tweet for user: " + user_id.to_s
@@ -85,13 +85,13 @@ class PGTest
   #returns all tweets with a given hashtag (incl assoc user)
   def lookup_hashtag (hashtag)
     # TODO: If bad performance, we might do a seondary query instead of a join
-    resp = @db.async_exec('SELECT * from tweets t INNER JOIN hashtags_tweets ht ON ht.tweet_id = t.id INNER JOIN users u ON t.user_id = u.id WHERE hashtag_id = $1', [hashtag])
+    resp = @db.exec('SELECT * from tweets t INNER JOIN hashtags_tweets ht ON ht.tweet_id = t.id INNER JOIN users u ON t.user_id = u.id WHERE hashtag_id = $1', [hashtag])
     debug 'hashtag: ' + hashtag.to_s + " had " + resp.count.to_s + " tweets"
   end
 
   #returns all tweets from a specific user
   def lookup_user (user_id)
-    resp = @db.async_exec('SELECT * from tweets t WHERE user_id = $1', [user_id])
+    resp = @db.exec('SELECT * from tweets t WHERE user_id = $1', [user_id])
     debug 'user: ' + user_id.to_s + " had " + resp.count.to_s + " tweets"
   end
 end
