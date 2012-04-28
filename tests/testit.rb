364fb623b3a41539f32ae5a413a1ea29e7b9a2a5
Bay <bgross17@gmail.com>
2012-04-28 09:25:41 -0400

    pool sizing

--- tests/testit.rb
+++ tests/testit.rb
@@ -16,7 +16,7 @@ require_relative 'threadPool.rb'
 @return_tweets_for_a_hashtag = 10
 
 #and how big is our thread pool?
-@pool_size = 40
+@pool_size = [1, 2, 4, 8, 16, 32]
 
 #------------------------------------------------------------------------------
 def main
@@ -63,60 +63,66 @@ def testSaddle( dbslug )
   #but get just as many hashtags as we need
   targets[:hashtags] =  @client.getHashtags( @num_hashtags ) 
 
-  #initialize thread pool
-  puts "- initializing thread pool of size #{@pool_size}..."
-  # establish new DB connection for each thread (except mongo which shares a pool)
-  if dbslug == :mongo
-    tpool = Pool.new(@pool_size, lambda {  } )
-  else
-    tpool = Pool.new(@pool_size, lambda { @client.connectDB } )
-  end
-
-  #generate a jobs list and randomly sort it
-  jobs = []
-  jobs += Array.new( @return_tweets_for_a_user, :user_lookup )
-  jobs += Array.new( @return_tweets_for_a_hashtag, :hash_lookup )
-  jobs += Array.new( @user_posts_a_new_tweet, :new_tweet )
-  jobs = jobs.sort{ rand }
-
-  ## Benchmarks begins here:----------------------------------------
-  puts "- starting benchmark now!"
-  puts Benchmark.measure { 
+  #allow for repeat benchmarking with variable pool sizes
+  @pool_size = [@pool_size] if !@pool_size.kind_of?(Array)
+  @pool_size.each do |cur_pool|
     
-    #iterate over all jobs
-    jobs.each do |j| 
-        
-      #calling appropriate method in a new thread
-      case j   
-      
-      #lookup user tweets    
-      when :user_lookup     
-        u = targets[:users].sample
-        tpool.schedule do
-          @client.lookup_user( u )
-        end       
+    #initialize thread pool
+    puts "- initializing thread pool of size #{cur_pool}..."
+    # establish new DB connection for each thread (except mongo which shares a pool)
+    if dbslug == :mongo
+      tpool = Pool.new(cur_pool, lambda {  } )
+    else
+      tpool = Pool.new(cur_pool, lambda { @client.connectDB } )
+    end
 
-      #lookup tweets by hashtag
-      when :hash_lookup
-        h = targets[:hashtags].sample
-        tpool.schedule do
-          @client.lookup_hashtag( h )
-        end
+    #generate a jobs list and randomly sort it
+    jobs = []
+    jobs += Array.new( @return_tweets_for_a_user, :user_lookup )
+    jobs += Array.new( @return_tweets_for_a_hashtag, :hash_lookup )
+    jobs += Array.new( @user_posts_a_new_tweet, :new_tweet )
+    jobs = jobs.sort{ rand }
+
+    ## Benchmarks begins here:----------------------------------------
+    puts "- starting benchmark now!"
+    puts Benchmark.measure { 
+    
+      #iterate over all jobs
+      jobs.each do |j| 
         
-      #post a new tweet 
-      when :new_tweet
-        u = targets[:users].sample
-        tpool.schedule do
-          @client.tweet( u )
+        #calling appropriate method in a new thread
+        case j   
+      
+        #lookup user tweets    
+        when :user_lookup     
+          u = targets[:users].sample
+          tpool.schedule do
+            @client.lookup_user( u )
+          end       
+
+        #lookup tweets by hashtag
+        when :hash_lookup
+          h = targets[:hashtags].sample
+          tpool.schedule do
+            @client.lookup_hashtag( h )
+          end
+        
+        #post a new tweet 
+        when :new_tweet
+          u = targets[:users].sample
+          tpool.schedule do
+            @client.tweet( u )
+          end
         end
+      
       end
       
-    end
-      
-    #then wait for threads to finish
-    tpool.shutdown
-  }
-  ##  Benchmark ends here: -------------------------------------------
+      #then wait for threads to finish
+      tpool.shutdown
+    }
+    ##  Benchmark ends here: -------------------------------------------
+    
+  end #end pool loop
 end
 
 # We don't want to reinclude the whole Generate() class on inserts
@@ -128,7 +134,7 @@ end
 
 #debug print function, turn on or off
 def debug( msg )
-  puts Time.now.strftime("- %I:%M%p: ") + msg
+  #puts Time.now.strftime("- %I:%M%p: ") + msg
 end
 
 #run it
